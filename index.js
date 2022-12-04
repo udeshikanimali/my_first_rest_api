1d4b596bee5ed180e5830a9a9a7f1869023c8972
Udeshika <udeshikanimali644@gmail.com>
2022-12-04 10:56:56 +0530

    Rest API

---
+++ index.js
@@ -0,0 +1,66 @@
+const express =require('express');
+const Joi = require('joi');
+const app = express();
+app.use(express.json());
+
+const customers=[
+    {
+        "id":1,
+        "name":"Gayan"
+    },
+    {
+        "id":2,
+        "name":"Sunil"
+    },
+    {
+        "id":3,
+        "name":"Amal"
+    },
+    {
+        "id":4,
+        "name":"Hiruni"
+    }
+];
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to my first REST API!");
+});
+ 
+app.get("/api/customers",(req,res)=>{
+    res.send(customers);
+});
+
+app.get("/api/customers/:id",(req,res)=>{
+    const customer =customers.find((c)=>c.id===parseInt(req.params.id))
+    if(!customer){
+        res.status(404).send('<h2>Oppz customer is not found for id'+req.params.id+'</h2>');
+    }else{
+        res.send(customer);
+    }
+});
+
+app.post("/api/customers",(req,res)=>{
+    const {error} = validateCustomer(req.body);
+
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    const newCustomer ={
+        id:customers.length+1,
+        name:req.body.name
+    }
+
+    customers.push(newCustomer);git 
+    res.send(customer);
+});
+
+function validateCustomer(customer){
+    const schema ={
+        title:Joi.string().min(3).max(10).required()
+    }
+    return Joi.validate(customer,schema);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>console.log(`Listening to port ${port}...`));
